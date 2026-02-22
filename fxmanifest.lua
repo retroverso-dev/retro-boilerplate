0d5cb6bf6665f4aafa3527da506b9d88939c3a7a
Cristian Paixão <cristian.paixao@outlook.com>
2026-02-21 23:49:00 -0300

    chore: init project structure

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'Nome do Script'
+author 'retroverso.dev'
+version '0.1.0'
+description 'Adicionar descrição'
+
+dependencies {
+    --'oxmysql', -- Descomentar se for usar banco de dados
+    'retro-kit'
+}
+
+ui_page 'web/dist/index.html'
+
+files {
+    'web/dist/**/*'
+}
+
+shared_scripts {
+    '@retro-kit/init.lua',
+    'shared/**/*.lua'
+}
+
+client_scripts {
+    "client/core/init.lua",
+    "client/**/*.lua"
+}
+
+server_scripts {
+    -- '@oxmysql/lib/MySQL.lua', -- Descomentar se for usar banco de dados
+    "server/core/init.lua",
+    "server/**/*.lua"
+}
